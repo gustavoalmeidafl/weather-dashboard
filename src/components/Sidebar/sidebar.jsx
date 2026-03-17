c29aceeeecaab16a5a73594e982347dbd476430c
Gustavo <gugagualfort@gmail.com>
2026-03-16 22:58:07 -0300

    add icons and dashboard sidebar structe

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import './Sidebar.css'
 
-const sidebar = () => {
+
+const Sidebar = () => {
   return (
-    <div>
-      
-    </div>
+      <nav className='side-bar'>
+        <di className="user-icon"><i className="pi pi-user"></i></di>
+        <div className='side_menu'><i className="pi pi-th-large"></i></div>
+
+        <div className="side_list"><ul>
+          <li><i className="pi pi-chart-bar"></i></li>
+          <li><i className="pi pi-users"></i></li>
+          <li><i className="pi pi-cog"></i></li>
+          </ul></div>
+
+        <div className="side_footer"> <i className="pi pi-sign-out"></i></div>
+      </nav>
+
   )
 }
 
-export default sidebar
+export default Sidebar
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Dashboard.css'
+import Sidebar from '../components/Sidebar/sidebar'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Sidebar/>
+    </div>
+  )
+}
+
+export default Dashboard
